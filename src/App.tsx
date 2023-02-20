45079d38232606732107fff3ebe8347c87fca8d1
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2023-02-20 14:51:01 +0300

    Chore

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,31 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import PersonalInfo from "./components/personalInfo";
 import PickAddons from "./components/pickAddons";
+import SelectPlan from "./components/selectPlan";
+
+import { useSelector } from "react-redux";
 
 function App() {
+  const page = useSelector((state: any) => state.counter.page);
+  const name = useSelector((state: any) => state.counter.name);
+  const conditionalRender = () => {
+    switch (page) {
+      case 1:
+        return <PersonalInfo />;
+      case 2:
+        return <SelectPlan />;
+      case 3:
+        return <PickAddons />;
+      default:
+        return <PersonalInfo />;
+    }
+  };
+  console.log({ page });
+  console.log({ name });
   return (
     <div className="App">
       <Navbar />
-      <PickAddons />
+      {conditionalRender()}
     </div>
   );
 }
